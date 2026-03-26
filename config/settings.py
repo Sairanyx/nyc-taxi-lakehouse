8bf6ec400f6339fad2c7402b376bda47e444655e
edu.eduardrednick <eduard.rednic@edu.turkuamk.fi>
2026-03-26 20:02:15 +0000

    Dockerfiles, settings and some small deletions

--- config/settings.py
+++ config/settings.py
@@ -11,3 +11,7 @@ SILVER_PATH = f"s3a://{BUCKET}/silver/clean_trips/"
 GOLD_PATH   = f"s3a://{BUCKET}/gold"
 
 REFERENCE_PATH = f"s3a://{BUCKET}/raw/taxi_zone_lookup.csv"
+
+MINIO_ENDPOINT = os.getenv("MINIO_ENDPOINT", "http://localhost:9000")
+MINIO_ACCESS   = os.getenv("MINIO_ROOT_USER")
+MINIO_SECRET   = os.getenv("MINIO_ROOT_PASSWORD")
